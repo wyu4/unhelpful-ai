971460830f23dc8071f94899b5910eaf29c26842
Wilson <wilsonyu657@gmail.com>
2025-03-15 16:01:59 -0400

    Add initial js files

---
+++ js/request.js
@@ -0,0 +1,11 @@
+var testEndpoint = 'https://raw.githubusercontent.com/wyu4/unhelpful-ai/refs/heads/main/README.md';
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
