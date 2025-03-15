0409e16f0eb837d7f6ec7b87c5e4fa911ca54d7a
Wilson <wilsonyu657@gmail.com>
2025-03-15 16:49:34 -0400

    Rewrite request.js

--- js/request.js
+++ js/request.js
@@ -1,17 +1,10 @@
 var testEndpoint = 'https://raw.githubusercontent.com/wyu4/unhelpful-ai/refs/heads/main/README.md';
 
-function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
+function sendRequest(endpoint, callback) {
+    var client = new XMLHttpRequest();
+    client.onreadystatechange = callback(client.readyState, client.status, client.responseText);
 
-function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+    // Send the request
+    client.open('GET', endpoint, true);
+    client.send(null);
 }
