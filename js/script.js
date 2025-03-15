970eaffd6f2deca6bc095f7b03a594092fca28fd
Wilson <wilsonyu657@gmail.com>
2025-03-15 17:13:52 -0400

    Add node

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-var level = 0;
+let level = 0;
+let debounce = false;
 
+function sendPrompt(prompt) {
+    if (debounce) return;
+    debounce = true;
+    level += 0.1;
+    
+
+    debounce = false;
+}
